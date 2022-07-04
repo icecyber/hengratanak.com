a7480dacc07e3aa24d1520845245168ef0b3b58c
icecyber <icecyber99@gmail.com>
2022-07-04 13:28:02 +0700

    first

---
+++ app.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/" , function(req , res){
+  res.render("index");
+});
+
+app.use(function (req, res, next) {
+  //Capture All 404 errors
+    res.status(404).render("404.ejs")
+  })
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
